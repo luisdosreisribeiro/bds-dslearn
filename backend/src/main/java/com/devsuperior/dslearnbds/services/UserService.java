9733dcf456888b0148766d1f6c494e1e5efdcb78
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-07-27 16:11:04 -0300

    Find By Id.

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslearnbds.dto.UserDTO;
 import com.devsuperior.dslearnbds.entities.User;
 import com.devsuperior.dslearnbds.repositories.UserRepository;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService implements UserDetailsService {
 	
@@ -17,6 +21,12 @@ public class UserService implements UserDetailsService {
 	
 	@Autowired
 	UserRepository repository;
+	
+	public UserDTO findById(Long id) {
+	Optional<User> obj=repository.findById(id);
+	User entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found"));
+	return new UserDTO(entity);
+	}
 	
 	
 	@Override
@@ -29,5 +39,7 @@ public class UserService implements UserDetailsService {
 		logger.info("User found " +username);
 		return user;
 	}
+	
+	
 
 }
